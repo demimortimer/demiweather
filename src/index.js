4720f5426fedd5c63e155bfccf0fd19679eb2432
demimortimer <153766264+demimortimer@users.noreply.github.com>
2023-12-22 19:52:57 +0000

    api data for weather conditions

--- src/index.js
+++ src/index.js
@@ -1,14 +1,45 @@
 function refreshWeather(response) {
+  console.log(response.data);
+
   let temperatureElement = document.querySelector("#weather-info-temp");
   let temperature = response.data.temperature.current;
   let cityElement = document.querySelector("#city");
 
+  let weatherDesc = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind-speed");
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
+
+  windElement.innerHTML = response.data.wind.speed;
+  humidityElement.innerHTML = response.data.temperature.humidity;
+  weatherDesc.innerHTML = response.data.condition.description;
   cityElement.innerHTML = response.data.city;
   temperatureElement.innerHTML = response.data.temperature.current;
-
   temperatureElement.innerHTML = Math.round(temperature);
+  timeElement.innerHTML = formatDate(date);
 }
 
+function formatDate(date) {
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${day} ${hours}:${minutes}`;
+}
 function searchCity(city) {
   let apiKey = "31044e85dc6dotb5a7ff80ae77ab2504";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
